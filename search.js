975fe6ba54f551f607ea69afc958d4cc3791dab0
DannyFung97 <32408586+DannyFung97@users.noreply.github.com>
2017-10-21 16:23:19 -0700

    findDif function added

--- search.js
+++ search.js
@@ -21,7 +21,7 @@ x% = abs(100((traitSum/maxSum)-opposite)), where opposite is either a 0 or 1
 console.log(userTwoDB.conscientiousness.summary.score)
 */
 
-
+function findDif(mode){
 var MAX_ATTRIBUTE_SCORE = 4;
 var difference;
 
@@ -29,13 +29,13 @@ if ( mode == "opposite") {
 	difference = algorithm(userOneInfo, userTwoInfo);	
 }  // returns 1 if "opposite" enough, returns 0 if not
 
-
 if ( mode == "similar") {
 	difference = 100 - algorithm(userOneInfo, userTwoInfo);
 }  // returns 0 if "opposite" enough, returns 1 if not
 
+return difference;
 	console.log("you match/dont match with user two based on your preferences (A,B,C) by x percent!");
-
+}
 
 // Returns the difference percentage for user one and user two
 function algorithm(userOneInfo, userTwoInfo) {
