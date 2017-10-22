6ded158486ebbf1f7288c853723103e01622eb22
Erika Joun <hjoun@ucsd.edu>
2017-10-22 02:16:35 -0700

    search.js

--- search.js
+++ search.js
@@ -1,40 +1,37 @@
 var MAX_ATTRIBUTE_SCORE = 4;
 var ALLOWED_VARIANCE = 70;
 var HUNDRED_PERCENT = 100;
-var findSimilarPartner = true;
+var findSimilarPartner = false;
+var percentage = 0;
 
-arr = []
 userOneInfo = [];
-
-// if user checks a check box, get the name/attribute of the check box
-//and stores it to an array
-function submit(){
-	var arr = document.getElementsByName("type");
-	arr.forEach(function(elem){
-		if (elem.checked) {
-			userOneInfo.push(elem);     
-		}
-	}
-	console.log("called submit()");
-}
 
 
 function findDif() {
 	findSimilarPartner = false;
-	document.write("called findDif()");
+	localStorage.setItem("findSimilarPartner", findSimilarPartner);
 }
 
 function findSame() {
 	findSimilarPartner = true;
-	document.write("called findSame()");
+	localStorage.setItem("findSimilarPartner", findSimilarPartner);
 }
 
-function report(percentage) {
-	if(findSimilarPartner == false) {
-		var percentage = algorithm(userOneInfo, MAX_ATTRIBUTE_SCORE, arr);	
+
+function report() {
+
+	var arr = document.getElementsByName("type");
+
+	arr.forEach(function(elem){
+		if (elem.checked) {
+			userOneInfo.push(elem);     
+		}
+	})
+	if(localStorage.getItem("findSimilarPartner") == false) {
+		percentage = algorithm(userOneInfo, MAX_ATTRIBUTE_SCORE, arr);	
 	}
-	if(findSimilarPartner == true) {
-		var percentage = HUNDRED_PERCENT - algorithm(userOneInfo, MAX_ATTRIBUTE_SCORE, arr);
+	else {
+		percentage = HUNDRED_PERCENT - algorithm(userOneInfo, MAX_ATTRIBUTE_SCORE, arr);
 	}
 
 	var matchRating = "";
@@ -44,31 +41,35 @@ function report(percentage) {
 	else {
 		matchRating += "You don't match ";
 	}
-	matchRating += "with user two based on your preferences by " + percentage + " percent!";
+	matchRating += "with user two based on your preferences by "
+	matchRating += percentage;
+	matchRating += " percent!";
 	document.write(matchRating);
 }
 
 // Returns the difference percentage for user one and user two
 function algorithm(userOneInfo, maxAttributeScore, arr) {
+
 	var numOfAttributes = userOneInfo.length;
 	var difference = 0;
 
 	// loop through arr to search for the attributes chosen by the user
-	for(var i = 0; i < arr.length; i++) {
-		// search through list of attributes
-		for(var i = 0; i < numOfAttributes; i++) {
-			// find attribute chosen by user
-			var attribute = userOneInfo[i];
-			if(attribute.toString() == arr[i]) {
-				var score1 = userOneDB[attribute.toString()].summary.score;
-				var score2 = userTwoDB[attribute.toString()].summary.score;
-			// calculate the difference
-			difference += abs(score1 - score2);
-			}
-	
-		}
-	}
+	for(var i = 0; i < numOfAttributes; i++) {
+		
+		var attribute = userOneInfo[i];
 
-	var differencePercentage = (double)difference/(numOfAttributes * maxAttributeScore) * HUNDRED_PERCENT;
+		var score1 = userOne[attribute.value].summary.score;
+		var score2 = userTwo[attribute.value].summary.score;
+
+		console.log(score1, score2);
+		// calculate the difference
+		difference += Math.abs(score1 - score2);
+
+	}
+	
+
+	var differencePercentage = difference/(numOfAttributes * maxAttributeScore) * HUNDRED_PERCENT;
+	differencePercentage = parseFloat(Math.round(differencePercentage * 100) / 100).toFixed(2);
 	return differencePercentage;
 }
+
