e74b772eee926dea69996a217891d2376d46274a
Erika Joun <hjoun@ucsd.edu>
2017-10-21 21:45:21 -0700

    search.js

--- search.js
+++ search.js
@@ -2,7 +2,6 @@ var userOneDB = require('./userOne.json');
 var userTwoDB = require('./userTwo.json');
 var MAX_ATTRIBUTE_SCORE = 4;
 var ALLOWED_VARIANCE = 70;
-var difference = 0;
 
 
 /**
@@ -62,20 +61,24 @@ function report(percentage) {
 }
 
 // Returns the difference percentage for user one and user two
-function algorithm(userOneInfo, maxAttributeScore) {
+function algorithm(userOneInfo, maxAttributeScore, arr) {
 	var numOfAttributes = userOneInfo.length;
 	var difference = 0;
 
-	//for each attribute
-	for(var i = 0; i < numOfAttributes; i++) {
-		// get the score for the attribute for userOne
-		var attribute = userOneInfo[i];
-		var score1 = userOneDB[attribute.toString()].summary.score;
-		var score2 = userTwoDB[attribute.toString()].summary.score;
-
-		// calculate the difference
-		difference += abs(score1 - score2);
-
+	// loop through arr to search for the attributes chosen by the user
+	for(var i = 0; i < arr.length; i++) {
+		// search through list of attributes
+		for(var i = 0; i < numOfAttributes; i++) {
+			// find attribute chosen by user
+			var attribute = userOneInfo[i];
+			if(attribute.toString() == arr[i]) {
+				var score1 = userOneDB[attribute.toString()].summary.score;
+				var score2 = userTwoDB[attribute.toString()].summary.score;
+			// calculate the difference
+			difference += abs(score1 - score2);
+			}
+	
+		}
 	}
 
 	var differencePercentage = (double)difference/(numOfAttributes * maxAttributeScore) * 100;
